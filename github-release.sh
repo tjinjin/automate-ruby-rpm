0562509e1d5ad5913336b6e2959bff7076411e26
tanaka masato <tanaka@feedforce.jp>
2015-10-29 17:27:39 +0900

    Fix syntax error orz...

--- github-release.sh
+++ github-release.sh
@@ -12,12 +12,13 @@ github-release release \
   --repo automate-ruby-rpm \
   --tag $VERSION \
   --name "Ruby-$VERSION" \
-  --description "$(cat template.txt)"
+  --description "not release"
 
 # upload files
-for i in $(ls -1 *.rpm) do
+for i in $(ls -1 *.rpm)
+do
   echo "\* $i" >> description.md
-  echo "  \* $(openssh sha256 $i)" >> description.md
+  echo "  \* $(openssl sha256 $i)" >> description.md
   github-release upload --user tjinjin \
     --repo automate-ruby-rpm \
     --tag $VERSION \
